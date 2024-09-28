633cfda8d28dfb23d641d4ace9fe630ad6783765
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-28 23:19:06 +0530

    feat: create a post route for sign up

--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -4,5 +4,8 @@ const signUpController = require("../controllers/signUpControllers");
 const signUpRouter = Router();
 
 signUpRouter.get("/", signUpController.getSignUpPage);
+signUpRouter.post("/", signUpController.createNewUser);
+
+
 
 module.exports = signUpRouter;
