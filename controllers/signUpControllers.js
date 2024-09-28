fdfe6b1f3dcf2af5ee93c640f7e520b4c6288fd0
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 02:30:10 +0530

    wip(validation): create middleware to check inputs

--- controllers/signUpControllers.js
+++ controllers/signUpControllers.js
@@ -1,10 +1,32 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require("express-validator");
+
 const db = require("../db/queries");
 
 const getSignUpPage = asyncHandler(async (req, res) => {
     res.render("pages/signUp", { title: "Sign up" });
 });
+
+const validateSignUp = [
+    body('first-name').trim().isLength({ min: 1 }).withMessage('First name is required'),
+    body('last-name').trim().isLength({ min: 1 }).withMessage('Last name is required'),
+    body('username').trim().isEmail().withMessage('Username must be a valid email')
+        .custom(async (value) => {
+            const user = await db.findUserByUsername(value);
+            if (user) {
+                throw new Error('Username already in use');
+            }
+            return true;
+        }),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
+    body('confirm-password').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password');
+        }
+        return true;
+    })
+];
 
 const createNewUser = asyncHandler(async (req, res, next) => {
     const { firstName, lastName, username, password } = req.body;
