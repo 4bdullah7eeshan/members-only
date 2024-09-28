6831e715f8b03d27c163048540b121c41c119774
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 05:22:36 +0530

    feat: create route and controllers for join submit

--- controllers/joinControllers.js
+++ controllers/joinControllers.js
@@ -1,11 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
 
 const getJoinForm = asyncHandler(async (req, res) => {
     res.render("pages/join", { title: "Join", user: req.user });
 });
 
+const giveMembershipStatus = asyncHandler(async (req, res) => {
+    const { passcode } = req.body;
+    const userId = req.user.id;
+
+    const correctPasscode = "Odin";
+
+    if (passcode.toLowerCase() === correctPasscode.toLowerCase()) {
+        await db.updateMembershipStatus(userId, true);
+        return res.redirect("/");
+    } else {
+        return res.render("pages/join", {
+            title: "Join",
+            user: req.user,
+            error: "Incorrect passcode. Please try again."
+        });
+    }
+});
+
 
 module.exports = {
     getJoinForm,
+    giveMembershipStatus,
 
 };
--- routes/joinRouter.js
+++ routes/joinRouter.js
@@ -4,5 +4,8 @@ const joinController = require("../controllers/joinControllers");
 const joinRouter = Router();
 
 joinRouter.get("/", joinController.getJoinForm);
+joinRouter.post("/", joinController.giveMembershipStatus);
+
+
 
 module.exports = joinRouter;
