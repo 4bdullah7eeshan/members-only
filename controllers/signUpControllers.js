0f8114a706b8fb860b67f28ba514b7d80a4c173e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 00:07:00 +0530

    feat: secure passwords with bcrypt

--- controllers/signUpControllers.js
+++ controllers/signUpControllers.js
@@ -5,16 +5,19 @@ const getSignUpPage = asyncHandler(async (req, res) => {
     res.render("pages/signUp", { title: "Sign up" });
 });
 
-const createNewUser = asyncHandler(async (req, res) => {
+const createNewUser = asyncHandler(async (req, res, next) => {
     const { firstName, lastName, username, password } = req.body;
-
     try {
-        await db.insertNewUser({ firstName, lastName, username, password });
-        res.redirect("/");
-    } catch(err) {
+        bcrypt.hash(password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return next(err); 
+            }
+            await db.insertNewUser({ firstName, lastName, username, hashedPassword });
+            res.redirect("/");
+        });
+    } catch (err) {
         return next(err);
     }
-    
 });
 
 module.exports = {
