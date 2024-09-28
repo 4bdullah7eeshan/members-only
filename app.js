d44a86d883d48fa7d1e11e4ad696ce6c778e0c7f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 01:36:20 +0530

    refactor: remove bcrypt import

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
 const signUpRouter = require("./routes/signUpRouter");
 
 
