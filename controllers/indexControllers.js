9b7ac61e0a9c78233740b8c2b203429e785174e2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 02:08:56 +0530

    fix: change object passing format

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const getHomePage = asyncHandler(async (req, res) => {
-    res.render("pages/index", { title: "Home" }, { user: req.user });
+    res.render("pages/index", { title: "Home", user: req.user });
 });
 
 
