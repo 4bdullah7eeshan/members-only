76e33189f10b228eaf630427e1b689b217f0c18a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 00:46:25 +0530

    feat: pass req object to the rendering page

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const getHomePage = asyncHandler(async (req, res) => {
-    res.render("pages/index", { title: "Home" });
+    res.render("pages/index", { title: "Home" }, { user: req.user });
 });
 
 
