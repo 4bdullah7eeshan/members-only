03e419ee53d275bb2308f7d566a96cc64fefb53e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-28 23:44:02 +0530

    wip: handling new user, change second name to last

--- controllers/signUpControllers.js
+++ controllers/signUpControllers.js
@@ -5,7 +5,20 @@ const getSignUpPage = asyncHandler(async (req, res) => {
     res.render("pages/signUp", { title: "Sign up" });
 });
 
+const createNewUser = asyncHandler(async (req, res) => {
+    const { firstName, lastName, username, password } = req.body;
+
+    try {
+        await db.insertNewUser({ firstName, lastName, username, password });
+        res.redirect("/");
+    } catch(err) {
+        return next(err);
+    }
+    
+});
+
 module.exports = {
     getSignUpPage,
+    createNewUser,
 
 };
--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,14 @@
 const pool = require("./pool");
 
+async function insertNewUser({ firstName, lastName, username, password }) {
+    const { rows } = await pool.query(
+        "INSERT INTO users (firstName, lastName, username, password) VALUES ($1, $2, $3, $4) RETURNING *",
+        [firstName, lastName, username, password]
+    );
+    return rows[0];
+}
 
 module.exports = {
+    insertNewUser,
 
 };
