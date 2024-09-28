ef88fd676526db03bf11528f83c688a0a0a528b8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 01:52:29 +0530

    feat: add sql script to populate tables in db

--- db/populateDb.js
+++ db/populateDb.js
@@ -4,7 +4,29 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const SQL = `
+-- Users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    membership_status BOOLEAN DEFAULT FALSE,
+    admin BOOLEAN DEFAULT FALSE
+);
 
+-- Messages
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,               
+    title VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)                   
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
 `;
 
 async function main() {
