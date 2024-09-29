cdf3706d9c3c4f09f71cd3d9ce6bb308e11cae34
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-29 09:01:17 +0530

    feat(ui): generate all messages based on status

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,7 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
+
 
 const getHomePage = asyncHandler(async (req, res) => {
-    res.render("pages/index", { title: "Home", user: req.user });
+    let messages;
+
+    if (req.user && req.user.membership_status) {
+        messages = await db.getAllMessagesWithUserDetails();
+    } else {
+        messages = await db.getAllMessagesWithoutUserDetails();
+    }
+
+    res.render("pages/index", { title: "Home", user: req.user, messages });
 });
 
 
